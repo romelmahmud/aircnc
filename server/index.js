1530346828275d735ae4b0032769ffa0a9d5e7d4
Romel Mahmud <romelmahmud.dev@gmail.com>
2022-11-26 12:29:19 +0600

    save user on server done

--- server/index.js
+++ server/index.js
@@ -1,38 +1,78 @@
-const express = require('express')
-const cors = require('cors')
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 
-const app = express()
-const port = process.env.PORT || 8000
+const app = express();
+const port = process.env.PORT || 8000;
 
 // middlewares
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 // Database Connection
-const uri = process.env.DB_URI
+const uri = process.env.DB_URI;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
-})
+});
 
 async function run() {
   try {
-    const homesCollection = client.db('aircncdb').collection('homes')
+    const categoriesCollection = client
+      .db("laptopMart")
+      .collection("categories");
+    const usersCollection = client.db("laptopMart").collection("users");
 
-    console.log('Database Connected...')
+    // get all categories
+    app.get("/categories", async (req, res) => {
+      const query = {};
+      const result = await categoriesCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    // save user
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      console.log(user.email);
+      // TODO: make sure you do not enter duplicate user email
+      // find existing user
+      const userQuery = {
+        email: user.email,
+      };
+      const existingUser = await usersCollection.findOne(userQuery);
+      if (existingUser) {
+        res.send("User already exist");
+        return;
+      }
+      // creating new user
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    // create json web token
+
+    app.get("/jwt", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      if (user) {
+        const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, {
+          expiresIn: "7d",
+        });
+        return res.send({ accessToken: token });
+      }
+      res.status(403).send({ accessToken: "" });
+    });
+
+    console.log("Database Connected...");
   } finally {
   }
 }
 
-run().catch(err => console.error(err))
-
-app.get('/', (req, res) => {
-  res.send('Server is running...')
-})
+run().catch((err) => console.error(err));
 
 app.listen(port, () => {
-  console.log(`Server is running...on ${port}`)
-})
+  console.log(`Server is running...on ${port}`);
+});
